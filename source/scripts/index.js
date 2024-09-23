0ed18710a988e48688fa10f9290ae5a6bd5ac731
romnosk <romnosk@gmail.com>
2024-09-23 09:39:26 +0300

    Подготовка к сдаче на допуск.

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,9 @@
 /* в этот файл добавляет скрипты*/
+const mainNav = document.querySelector('.main-nav');
+const menuToggle = document.querySelector('.main-nav__toggle');
+
+mainNav.classList.remove('main-nav--nojs');
+mainNav.classList.add('main-nav--closed');
+menuToggle.addEventListener('click', () =>{
+  mainNav.classList.toggle('main-nav--closed');
+});
