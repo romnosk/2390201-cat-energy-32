2115cc926fd1f1c67a3dd1abd967e493e25dadf6
romnosk <romnosk@gmail.com>
2024-09-25 12:49:48 +0300

    Исправления по замечаниям на допуск

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -3,6 +3,7 @@ const mainNav = document.querySelector('.main-nav');
 const menuToggle = document.querySelector('.main-nav__toggle');
 
 mainNav.classList.remove('main-nav--nojs');
+mainNav.classList.add('main-nav--closed');
 menuToggle.addEventListener('click', () =>{
   mainNav.classList.toggle('main-nav--closed');
 });
