2f5f844ed8ff14aed4f2d434ad2c4899644af161
romnosk <romnosk@gmail.com>
2024-09-25 09:50:30 +0000

    Deploying to gh-pages from @ romnosk/2390201-cat-energy-32@2115cc926fd1f1c67a3dd1abd967e493e25dadf6 🚀

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,1 @@
-var e=document.querySelector(".main-nav"),n=document.querySelector(".main-nav__toggle");e.classList.remove("main-nav--nojs");n.addEventListener("click",()=>{e.classList.toggle("main-nav--closed")});
+var e=document.querySelector(".main-nav"),n=document.querySelector(".main-nav__toggle");e.classList.remove("main-nav--nojs");e.classList.add("main-nav--closed");n.addEventListener("click",()=>{e.classList.toggle("main-nav--closed")});
