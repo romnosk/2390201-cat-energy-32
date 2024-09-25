9bffaccc855ae6b6baa4fa2ff36f26191983590c
romnosk <romnosk@users.noreply.github.com>
2024-09-25 05:58:27 +0000

    Deploying to gh-pages from @ romnosk/2390201-cat-energy-32@d63020a890c0767424d88bb4d46b6febdce5d4af 🚀

---
+++ 17/scripts/index.js
@@ -0,0 +1,1 @@
+var e=document.querySelector(".main-nav"),n=document.querySelector(".main-nav__toggle");e.classList.remove("main-nav--nojs");n.addEventListener("click",()=>{e.classList.toggle("main-nav--closed")});
